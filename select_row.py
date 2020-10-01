9362a0fec3e4d7813706c3a1141cccc189e93941
Deshar <deshar_wor@yahoo.com>
2020-10-01 19:10:27 +0700

    First Test

--- select_row.py
+++ select_row.py
@@ -2,28 +2,16 @@ import csv
 
 def main():
 
-    y = open('TEST_CSV.csv', 'r')
+    x = open('TEST_CSV.csv', 'r')
+    y = open('output.csv','w')
     reader = csv.reader(y, delimiter=',')
 
-    for k in reader:
-        x = open('Master Config.txt','r')
-        print (k[0])
-        z = open('{0}.txt'.format(k[0]),'w')
-        line = 1
-        for i in x:
-            if line==9:
-                z.write('                 address {0};\n'.format(k[1]))
-            elif line==16:
-                z.write('                 address {0};\n'.format(k[2]))
-            elif line==23:
-                z.write('                 address {0};\n'.format(k[3]))
-            else:
-                z.write(i)
-            line+=1
-        x.close()
-        z.close()
+    for i in x:
+
+         y.write(i)
 
     y.close()
+    x.close()
 
 if __name__=="__main__":
     main()
